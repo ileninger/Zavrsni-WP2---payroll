ef76bf56af0a32f090060c5cc9b4f1d51002d5e9
Ivan Leninger <ileninger@live.com>
2023-11-21 20:47:12 +0100

    Mnozina i maknuti odbici
    Mnozina i maknuti odbici

--- PayRoll.sql
+++ PayRoll.sql
@@ -21,7 +21,7 @@ IBAN varchar (50) not null,
 OiB char (11) not null
 );
 
-create table ObracunRadnihSati (
+create table ObracunPlace (
 ObracunId int not null primary key identity (1,1),
 Radnik int not null,
 DatumObracuna date not null,
@@ -30,35 +30,36 @@ CijenaRadnogSata decimal (18,2) not null,
 IznosObracuna decimal(18,2) not null
 );
 
-create table Odbici (
-OdbiciId int not null primary key identity (1,1),
-Radnik int not null,
-UkupniOdbici decimal (18,2) not null,
-OsnovicaZaObracun decimal(18,2),
-MiO_I decimal (18,2),
-MiO_II decimal (18,2),
-PorezNaDohodak decimal (18,2)
-);
+--create table Odbici (
+--OdbiciId int not null primary key identity (1,1),
+--Radnik int not null,
+--UkupniOdbici decimal (18,2) not null,
+--OsnovicaZaObracun decimal(18,2),
+--MiO_I decimal (18,2),
+--MiO_II decimal (18,2),
+--PorezNaDohodak decimal (18,2)
+--);
 
-create table Placa (
+create table Place (
 PlacaId int not null primary key identity (1,1),
 ObracunSatiId int not null,
 OdbiciId int not null,
 --IznosPlace decimal (18,2) not null,
 --IznosOdbitaka decimal (18,2) not null,
+NazivPlace varchar (50),
 IznosIsplate decimal (18,2) not null
 );
 
-create table Isplata  (
+create table Isplate  (
 IsplataId int not null primary key identity (1,1),
 Radnik int not null,
 PlacaId int not null,
 IznosIsplate decimal (18,2) not null
 );
 
-alter table ObracunRadnihSAti add foreign key (Radnik) references Radnici (radnikId);
-alter table Odbici add foreign key (Radnik) references Radnici (radnikId);
-alter table Isplata add foreign key (Radnik) references Radnici (radnikId);
-alter table Placa add foreign key (ObracunSatiId) references ObracunRadnihSati(ObracunId);
-alter table Placa add foreign key (OdbiciId) references Odbici(OdbiciId);
-alter table Isplata add foreign key (PlacaId) references Placa(PlacaId);
+alter table ObracunPlace add foreign key (Radnik) references Radnici (radnikId);
+--alter table Odbici add foreign key (Radnik) references Radnici (radnikId);
+alter table Isplate add foreign key (Radnik) references Radnici (radnikId);
+alter table Place add foreign key (ObracunSatiId) references ObracunPlace(ObracunId);
+--alter table Place add foreign key (OdbiciId) references Odbici(OdbiciId);
+alter table Isplate add foreign key (PlacaId) references Place(PlacaId);
