00d0db4277cc7fccea1b654e3565568daa4c7198
Ivan Leninger <ileninger@live.com>
2024-02-08 21:17:17 +0100

    Kreirao sam glavni izbornik i dodao web api project

---
+++ C#/ZavršniRad/ZavršniRad/KonzolnaAplikacijaUjednojKlasi/Pomocno.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZavršniRad.KonzolnaAplikacijaUjednojKlasi
+{
+    internal class Pomocno
+    {
+        public static int UcitajRasponBrojeva(string poruka, string greska, int poc, int kraj)
+        {
+            int i;
+            while (true)
+            {
+                Console.Write(poruka);
+
+                try
+                {
+                    i = int.Parse(Console.ReadLine());
+                    if (i >= poc && i <= kraj)
+                    {
+                        return i;
+                    }
+
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(greska);
+                }
+            }
+
+        }
+    }
+}
