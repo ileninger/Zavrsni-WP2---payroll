7e54e245cc26771e896db1f3f09ebf04141dbc8f
Ivan Leninger <ileninger@live.com>
2024-02-08 21:52:02 +0100

    Dodano ucitavanje cijelog broja,decimalni, datuma i stringa

--- C#/ZavršniRad/ZavršniRad/KonzolnaAplikacijaUjednojKlasi/Pomocno.cs
+++ C#/ZavršniRad/ZavršniRad/KonzolnaAplikacijaUjednojKlasi/Pomocno.cs
@@ -30,7 +30,86 @@ namespace ZavršniRad.KonzolnaAplikacijaUjednojKlasi
                     Console.WriteLine(greska);
                 }
             }
-
         }
+        
+        public static int UcitajCijeliBroj(string poruka, string greska)
+        {
+            int i;
+            while (true)
+            {
+                Console.Write(poruka);
+                try
+                {
+                    i = int.Parse(Console.ReadLine());
+                    if (i > 0)
+                    {
+                        return i;
+                    }
+                    Console.WriteLine(greska);
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(greska);
+                }
+            }
+        }
+
+        public static decimal UcitajDecimalnibroj (string poruka, string greska)
+        {
+            decimal i;
+            {
+                while (true)
+                {
+                    Console.Write(poruka);
+                    try
+                    {
+                        i = decimal.Parse(Console.ReadLine());
+                        if (i > 0)
+                        {
+                            return i;
+                        }
+                        Console.WriteLine(greska);
+                    }
+                    catch (Exception ex)
+                    { 
+                        Console.WriteLine(greska);
+                    }
+                }
+            }
+        }
+
+        public static string UcitajString (string poruka, string greska)
+        {
+            string s = "";
+
+            while (true)
+            {
+                Console.Write(poruka);
+                s=Console.ReadLine();
+                if(s != null && s.Trim().Length > 0)
+                {
+                    return s;
+
+                }
+                Console.WriteLine(greska);
+            }
+        }
+
+        internal static DateOnly UcitajDatum(string poruka, string greska)
+        {
+            while (true)
+            {
+                Console.Write(poruka);
+                try
+                {
+                   return DateOnly.Parse(Console.ReadLine());   
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(greska);
+                }
+            }
+        } 
     }
 }
