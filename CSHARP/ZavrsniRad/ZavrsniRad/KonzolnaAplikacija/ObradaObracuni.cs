ed50f65c8d4ecd074eb68cdc55ac277e5aae89a3
Ivan Leninger <ileninger@live.com>
2024-02-11 11:02:50 +0100

    Dodana metoda PrikaziIzbornik

--- CSHARP/ZavrsniRad/ZavrsniRad/KonzolnaAplikacija/ObradaObracuni.cs
+++ CSHARP/ZavrsniRad/ZavrsniRad/KonzolnaAplikacija/ObradaObracuni.cs
@@ -3,10 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ZavrsniRad.KonzolnaAplikacija.Model;
 
 namespace ZavrsniRad.KonzolnaAplikacija
 {
     internal class ObradaObracuni
     {
+        public List <Odbitak> Odbici {  get; }
+
+        public void PrikaziIzbornik()
+        {
+            Console.WriteLine("***********************************************************");
+            Console.WriteLine("Odaberite jedanu od ponuđenih mogučnosti rada s obračunima ");
+            Console.WriteLine("***********************************************************");
+            Console.WriteLine("1. Prikaži sve obračune ");
+            Console.WriteLine("2. Dodaj obračun ");
+            Console.WriteLine("3. Izmjeni obračun ");
+            Console.WriteLine("4. Obriši obračun ");
+            Console.WriteLine("5. Povratak na prethodni izbornik");
+
+            OdabirIzbornikRadaSaObracunima();
+
+        }
+
+        private void OdabirIzbornikRadaSaObracunima()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
