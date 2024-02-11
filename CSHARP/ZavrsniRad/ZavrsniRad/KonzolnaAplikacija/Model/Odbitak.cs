71f5b9b45d4fb418644b3c08c005dba4fed56de0
Ivan Leninger <ileninger@live.com>
2024-02-11 11:01:36 +0100

    Klasa odbici promjenjena ime u odbitak

---
+++ CSHARP/ZavrsniRad/ZavrsniRad/KonzolnaAplikacija/Model/Odbitak.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZavršniRad.KonzolnaAplikacija.Model;
+
+namespace ZavrsniRad.KonzolnaAplikacija.Model
+{
+    internal class Odbitak:Entitet
+    {
+        public decimal OsnovniOsobniOdbitak { get; set; }
+
+        public decimal PostotakZaPrviMirovnisnkiStup { get; set; }
+
+        public decimal PostotakZaDrugiMirovnisnkiStup { get; set; }
+    }
+}
