778983032e15ead59303ac72c3934d7674b08271
Ivan Leninger <ileninger@live.com>
2024-02-11 10:20:46 +0100

    Dodane klase obračun i odbici
    Klasa odbici obahvačeju unose postotaka za MIO I i MIO II te iznos Osnovnog osobnog odbitaka

---
+++ CSHARP/ZavrsniRad/ZavrsniRad/KonzolnaAplikacija/Model/Obracun.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+using ZavršniRad.KonzolnaAplikacija.Model;
+
+namespace ZavrsniRad.KonzolnaAplikacija.Model
+{
+    internal class Obracun:Odbici
+    {
+        public List<Radnik> Radnici { get; set; }
+        public DateTime DatumObracuna { get; set; }
+        public int BrojRadnihSati { get; set; }
+
+        public decimal CijenaRadnogSata { get; set; }
+
+        public decimal KoeficijentRadnogMjesta { get; set; }
+
+        //Bruto I = CijenaRadnogSata*BrojRadnihSati*KoeficijentRadnnogMjesta
+        public decimal BrutoI { get; set; }
+
+        //BrurtoII = BrutoI - (MIO I + MIO II) - služi kao porezna osnovica
+        public decimal BrutoII { get; set; }
+
+
+        public decimal NetoIznosZaIsplatuRadniku { get; set; }
+
+        public override string ToString()
+        {
+            return "\n Datum obračuna: " + DatumObracuna + "\n Cijena radnog sata " + CijenaRadnogSata  ;
+        }
+
+
+
+
+    }
+}
