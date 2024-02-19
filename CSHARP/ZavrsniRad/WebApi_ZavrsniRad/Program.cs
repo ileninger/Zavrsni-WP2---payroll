3993a7690eebb590f55448039e8e84e86232b09c
Ivan Leninger <ileninger@live.com>
2024-02-19 13:34:40 +0100

    Digao sam predispozicije za web api u novom projektu

--- CSHARP/ZavrsniRad/WebApi_ZavrsniRad/Program.cs
+++ CSHARP/ZavrsniRad/WebApi_ZavrsniRad/Program.cs
@@ -1,3 +1,5 @@
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +7,31 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(sgo =>
+{ // sgo je instanca klase SwaggerGenOptions
+  // čitati https://devintxcontent.blob.core.windows.net/showcontent/Speaker%20Presentations%20Fall%202017/Web%20API%20Best%20Practices.pdf
+    var o = new Microsoft.OpenApi.Models.OpenApiInfo()
+    {
+        Title = "Obracun plača WebApi",
+        Version = "v1",
+        Contact = new Microsoft.OpenApi.Models.OpenApiContact()
+        {
+            Email = "ileninger@live.com",
+            Name = "Ivan Leninger"
+        },
+        Description = "Ovo je dokumentacija za Obračun plača API",
+        License = new Microsoft.OpenApi.Models.OpenApiLicense()
+        {
+            Name = "Edukacijska licenca"
+        }
+    };
+    sgo.SwaggerDoc("v1", o);
+
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    sgo.IncludeXmlComments(xmlPath, includeControllerXmlComments: true);
+
+});
 
 var app = builder.Build();
 
@@ -23,3 +49,4 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
