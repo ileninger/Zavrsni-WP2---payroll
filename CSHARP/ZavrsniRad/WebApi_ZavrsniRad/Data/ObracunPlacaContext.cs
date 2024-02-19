f817b0ff8fc76b3dcedb6060ef0c1c8c1ba6a1fa
Ivan Leninger <ileninger@live.com>
2024-02-19 15:12:52 +0100

    Spajanje baze napravljene osnove

---
+++ CSHARP/ZavrsniRad/WebApi_ZavrsniRad/Data/ObracunPlacaContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApi_ZavrsniRad.Models;
+
+namespace WebApi_ZavrsniRad.Data
+{
+    /// <summary>
+    /// Ovo mi je datoteka gdje ću navoditi datasetove i načine spajanja u bazi
+    /// </summary>
+    public class ObracunPlacaContext:DbContext
+    {
+        /// <summary>
+        /// Konstruktor proslijeđujemo konstruktor gore
+        /// </summary>
+        /// <param name="options"></param>
+        public ObracunPlacaContext(DbContextOptions<ObracunPlacaContext> options):
+            base(options)
+        {
+
+        }
+        /// <summary>
+        /// Radnici u bazi
+        /// </summary>
+        public DbSet<Radnik> Radnici { get; set; }
+
+    }
+}
