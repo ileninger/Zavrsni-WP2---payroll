d09e6717955a1d2ca664aa3daf76cabf6cb8ac19
Ivan Leninger <ileninger@live.com>
2024-02-19 15:13:18 +0100

    Definirana klasa Radnik i sta mora imat

---
+++ CSHARP/ZavrsniRad/WebApi_ZavrsniRad/Models/Radnik.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApi_ZavrsniRad.Models
+{
+    /// <summary>
+    /// Ovo mi je POCO koji je mapiran na bazu 
+    /// </summary>
+    public class Radnik:Entitet
+    {
+        /// <summary>
+        /// Ime radnika u bazi 
+        /// </summary>
+        [Required(ErrorMessage ="Ime radnika je obavezno")]
+        public string? Ime { get; set; }
+    }
+}
+
+
+//public string Ime { get; set; }
+
+//public string Prezime { get; set; }
+
+//public string OiB { get; set; }
+
+//public DateTime DatumZaposlenja { get; set; }
+
+//public string Iban { get; set; }
