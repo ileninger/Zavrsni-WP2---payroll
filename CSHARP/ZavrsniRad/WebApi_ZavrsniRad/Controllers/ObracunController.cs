8268eb7aa25bffbc1dbabd7842dea36383b6ff03
Ivan Leninger <ileninger@live.com>
2024-02-26 18:39:47 +0100

    Kreirao sam novi kontloler obracun i dodao u prikaz

---
+++ CSHARP/ZavrsniRad/WebApi_ZavrsniRad/Controllers/ObracunController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApi_ZavrsniRad.Data;
+using WebApi_ZavrsniRad.Models;
+namespace WebApi_ZavrsniRad.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class ObracunController:ControllerBase
+    {
+        private readonly ObracunPlacaContext _context;
+
+        public ObracunController(ObracunPlacaContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+
+        public IActionResult Get()
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            try
+            {
+                var obracuni = _context.Obracuni.ToList();
+                if(obracuni==null || obracuni.Count == 0)
+                {
+                    return new EmptyResult();
+                }
+                return new JsonResult(obracuni);
+            }catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status503ServiceUnavailable, ex.Message);
+            }
+
+        }
+             
+
+    }
+}
