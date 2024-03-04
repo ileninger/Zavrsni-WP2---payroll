64cff452520f3ffd1a07334dda5dba322eeccb9f
Ivan Leninger <ileninger@live.com>
2024-03-04 16:48:18 +0100

    Napravio sam izbornik

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom"
 import Pocetna from "./pages/Pocetna"
 import { RoutesNames } from "./constants"
+import NavBar from "./components/NavBar"
 
 function App() {
 
   return (
     <>
+      <NavBar />
       <Routes>
         <Route path={RoutesNames.HOME} element ={<Pocetna/>} />
       </Routes>
---
+++ Frontend/src/components/NavBar.jsx
@@ -0,0 +1,43 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import { useNavigate } from 'react-router-dom';
+import { RoutesNames } from '../constants';
+
+import './NavBar.css';
+
+function NavBar() {
+
+    const navigate = useNavigate ();
+
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand 
+            className='linkPocetna'
+            onClick={()=>navigate(RoutesNames.HOME)}
+            >
+                Obračun plača APP
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <NavDropdown title="Izbornik" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Radnici</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Obračuni</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Plače</NavDropdown.Item>
+              <NavDropdown.Divider />
+            </NavDropdown>
+            
+          </Nav>
+        </Navbar.Collapse>
+        <Navbar.Collapse className='justify-content-end'>
+            <Nav.Link target ='_blank' href='https://ileninger-001-site1.anytempurl.com/swagger/index.html'>API Dokumentacija</Nav.Link>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavBar;
