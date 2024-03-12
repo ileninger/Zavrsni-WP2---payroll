a05ab4f420fb565327021464e2c4bca6a0046a50
Ivan Leninger <ileninger@live.com>
2024-03-12 21:21:09 +0100

    Dodavanje radnika do Service

--- Frontend/src/pages/radnici/RadniciDodaj.jsx
+++ Frontend/src/pages/radnici/RadniciDodaj.jsx
@@ -1,118 +1,89 @@
-import { Button, Col, Container, Form, Row, } from "react-bootstrap";
-import { Link,useNavigate } from "react-router-dom";
-import { RiArrowGoBackFill, RiArrowGoForwardFill } from "react-icons/ri";
+import { Button, Col, Container, Form, Row} from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { RiArrowGoBackFill } from "react-icons/ri"
+import { RiArrowGoForwardFill } from "react-icons/ri";
 import { RoutesNames } from "../../constants";
-import RadnikService from "../../services/RadnikService";
-
 
 
 export default function RadniciDodaj (){
 
-    const navigate = useNavigate();
-
-    async function dodajRadnika(radnik){
-        const odgovor = await RadnikService.dodajRadnika(radnik);
-        if(odgovor.ok){
-            navigate(RoutesNames.RADNICI_PREGLED);
-        }else{
-            console.log(odgovor);
-            alert(odgovor.poruka);
-        }
-    }
-    function formatirajDatum(datum) {
-        const formatiraniDatum = new Date(datum).toISOString().split('T')[0];
-        return formatiraniDatum;
-      }
-
-    function handleSubmit(e){
+    function handleSubmit(e)
+    {
         e.preventDefault();
         const podaci = new FormData(e.target);
+        console.log(podaci.get('ime'));
 
-        //console.log(podaci.get('ime'));
-
-        const radnik = {
-
-            ime: podaci.get ('ime'),
+        const radnik = 
+        {
+            ime: podaci.get('ime'),
             prezime: podaci.get('prezime'),
             oiB: podaci.get('oib'),
-            datumZaposlenja:formatirajDatum(podaci.get('datumzaposlenja')),
-            iban: podaci.get('iban')
+            datumZaposlenja: podaci.get('datumzaposlenja'),
+            iban: podaci.get('iban'),
         };
 
-        //console.log(JSON.stringify(radnik));
+        console.log(JSON.stringify(radnik));
 
-        dodajRadnika(radnik)
-        
-    }   
+    }
+
+    
+
+
+
     
     return (
         <Container>
-           <Form onSubmit={handleSubmit}>
-                
+            <Form onSubmit={handleSubmit}>
                 <Form.Group controlId="ime">
                     <Form.Label>Ime</Form.Label>
                     <Form.Control 
                         type="text"
-                        name="ime"        
-                    />
+                        name="ime"/>
                 </Form.Group>
                 <Form.Group controlId="prezime">
                     <Form.Label>Prezime</Form.Label>
                     <Form.Control 
                         type="text"
-                        name="prezime"                        
-                    />
+                        name="prezime"/>
                 </Form.Group>
-
                 <Form.Group controlId="oib">
                     <Form.Label>OiB</Form.Label>
                     <Form.Control 
                         type="text"
-                        name="oib"                        
-                    />
+                        name="oib"/>
                 </Form.Group>
                 <Form.Group controlId="datumzaposlenja">
-                    <Form.Label>Datumzaposlenja</Form.Label>
+                    <Form.Label>DatumZaposlenja</Form.Label>
                     <Form.Control 
                         type="text"
-                        name="datumzaposlenja"                        
-                    />
+                        name="datumzaposlenja"/>
                 </Form.Group>
                 <Form.Group controlId="iban">
-                    <Form.Label>IBAN</Form.Label>
+                    <Form.Label>Iban</Form.Label>
                     <Form.Control 
                         type="text"
-                        name="iban"                        
-                    />
+                        name="iban"/>
                 </Form.Group>
-                <Row className="akcije">
-                    <Col>
-                        <Link 
-                        className="btn  btn-danger"
-                        to = {RoutesNames.RADNICI_PREGLED}>
-                        <RiArrowGoBackFill 
-                        size ={15}
-                        />
-                        Odustani
-                        </Link>
-                    </Col>
-                    <Col>
-                        <Button
-                            variant="primary"
-                            type="submit"
-                        >
-                            <RiArrowGoForwardFill
-                                size ={15}
-                            />
-                            Dodaj smjer
-                        </Button>
-     
-                    </Col>
-                </Row>
+            <Row className="akcije">
+                <Col>
+                    <Link 
+                        className="btn btn-danger"
+                        to={RoutesNames.SMJEROVI_PREGLED}>
+                        <RiArrowGoBackFill size={15} />    
+                    Odustani
+                    </Link>
+                </Col>
+                <Col>
+                    <Button
+                        variant="primary"
+                        type="submit">
+                        <RiArrowGoForwardFill size ={15} />
+                    Dodaj smjer
+                    </Button>
+                </Col>
+            </Row>
             </Form>
-        </Container>
-
-
+         </Container>
 
     );
 }
