b4c27cf8ab37b440cb8ae2020067bc9cb96015fe
Ivan Leninger <ileninger@live.com>
2024-03-13 20:59:09 +0100

    Dodan charset=utf-8 sada radi slanje podataka

--- Frontend/src/services/httpService.js
+++ Frontend/src/services/httpService.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const httpService=axios.create({
     baseURL:'https://ileninger-001-site1.anytempurl.com/api/v1',
     headers:{
-        'Content-Type':'aplication/json'
+        'Content-Type':'application/json; charset=utf-8'
     }
 });
 
