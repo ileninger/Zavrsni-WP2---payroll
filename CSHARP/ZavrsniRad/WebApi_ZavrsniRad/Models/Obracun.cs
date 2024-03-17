d2b5d27dd321496eb0f962a9aa18d13a6a13259d
Ivan Leninger <ileninger@live.com>
2024-03-17 21:07:32 +0100

    Produkcija MVP

--- CSHARP/ZavrsniRad/WebApi_ZavrsniRad/Models/Obracun.cs
+++ CSHARP/ZavrsniRad/WebApi_ZavrsniRad/Models/Obracun.cs
@@ -14,18 +14,6 @@ namespace WebApi_ZavrsniRad.Models
         /// </summary>
         [Required(ErrorMessage = "Broj radnih sati koje je radnik odradio u razdoblju obračuna je obavezan")]
         public decimal? BrojRadnihSati { get; set; }
-        /// <summary>
-        /// Prezime radnika u bazi
-        /// </summary>
-        [Required(ErrorMessage = "Jedinična cijena radnog sata ja obavezna")]
-        public decimal? CijenaRadnogSata { get; set; }
-
-        /// <summary>
-        /// Prezime radnika u bazi
-        /// </summary>
-        [Required(ErrorMessage = "Koeficijent radnog mjesta je obavezan")]
-        public decimal? KoeficijentRadnogMjesta { get; set; }
-
         //Bruto I = CijenaRadnogSata*BrojRadnihSati*KoeficijentRadnnogMjesta
         public decimal? BrutoI { get; set; }
 
